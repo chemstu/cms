47173d1d2b709563ccd2796589cd58e834b8194f
Your Name <you@example.com>
2018-06-05 22:40:06 +0800

    add captcha

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Mews\Captcha\CaptchaServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -208,6 +208,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,  /*验证码*/
 
     ],
 

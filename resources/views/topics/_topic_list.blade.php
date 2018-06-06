4f91580c045ff1b207281f4c80ce15525db1f6d8
Your Name <you@example.com>
2018-06-06 21:22:07 +0800

    Auto translate slug

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -14,7 +14,7 @@
 
 
                     <div class="media-heading">
-                        <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+                        <a href="{{ $topic->link() }}" title="{{ $topic->title }}">
                             {{ $topic->title }}
                         </a>
                         <a class="pull-right" href="{{ route('topics.show', [$topic->id]) }}" >

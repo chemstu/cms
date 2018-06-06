46764f517254af3b9190bf0981cfc5dae1a477b1
Your Name <you@example.com>
2018-06-06 22:08:15 +0800

    user can create reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,7 +11,10 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        //话题数+1
+        $reply->topic->increment('reply_count', 1);
+       // xss攻击
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)

aa580ca40686ccf7c6dd2c29a02efecfee6ca995
Your Name <you@example.com>
2018-06-06 21:57:39 +0800

    list user replies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+
         Schema::defaultStringLength(191);  /*解决数据无法迁移问题，5.7.7以上没有改问题*/
         \Carbon\Carbon::setLocale('zh'); /*中文显示时间错*/
         Topic::observe(TopicObserver::class);//原來的教程是錯誤的，必須加這計
